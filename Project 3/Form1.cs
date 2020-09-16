569f608fa34cfe72ab726122e2d369fc59ad8494
Tsulukidze <62519788+Tsulukidze@users.noreply.github.com>
2020-09-16 20:40:56 +0400

    Add files via upload

---
+++ Project 3/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Buzdolab fridge = new Buzdolab("fridge", "Philips", "P-2020", "smart", 3500, 0, 500, "A++");
+
+            Ceptel mobile = new Ceptel("Galaxy", "Samsung", "S8", "edge", 2500, 0, 64, 4, 3400);
+
+            LedTV tv = new LedTV("tv", "Sony", "Z-5", "Smart", 4000, 0, 55, "Full HD");
+
+            Laptop comp = new Laptop("E-5", "Acer", "Aspire", "Nvidia", 5000, 0, 15, "Full HD", 1000, 8, 2500);
+
+
+
+            label4.Text = tv.StokAdedi.ToString();
+            label6.Text = fridge.StokAdedi.ToString();
+            label11.Text = comp.StokAdedi.ToString();
+            label16.Text = mobile.StokAdedi.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            LedTV TV = new LedTV("tv", "Sony", "Z-5", "Smart", 4000, Convert.ToInt32(numericUpDown1.Value), 55, "Full HD");
+            TV.StokAdedi = Convert.ToInt32(label4.Text) - TV.Secilenadet;
+            double KDV_1 = TV.Hamfiyat * TV.Secilenadet * 1.18;
+
+            if (TV.Secilenadet <= TV.StokAdedi && TV.Secilenadet > 0)
+            {
+                label4.Text = TV.StokAdedi.ToString();
+                listBox1.Items.Add(TV.Secilenadet);
+                listBox2.Items.Add(TV.Ad);
+                listBox3.Items.Add(KDV_1);
+            }
+
+
+            Buzdolab BZ = new Buzdolab("Buzdolabi", "Philips", "P-2020", "smart", 3500, Convert.ToInt32(numericUpDown2.Value), 500, "A++");
+            BZ.StokAdedi = Convert.ToInt32(label6.Text) - BZ.Secilenadet;
+            double KDV_2 = BZ.Hamfiyat * BZ.Secilenadet * 1.05;
+
+            if (BZ.Secilenadet <= BZ.StokAdedi && BZ.Secilenadet > 0)
+            {
+                label6.Text = BZ.StokAdedi.ToString();
+                listBox1.Items.Add(BZ.Secilenadet);
+                listBox2.Items.Add(BZ.Ad);
+                listBox3.Items.Add(KDV_2);
+            }
+
+
+            Laptop LP = new Laptop("Laptop", "Acer", "Aspire", "Nvidia", 5000, Convert.ToInt32(numericUpDown3.Value), 15, "Full HD", 1000, 8, 2500);
+            LP.StokAdedi = Convert.ToInt32(label11.Text) - LP.Secilenadet;
+            double KDV_3 = LP.Hamfiyat * LP.Secilenadet * 1.15;
+
+            if (LP.Secilenadet <= LP.StokAdedi && LP.Secilenadet > 0)
+            {
+                label11.Text = LP.StokAdedi.ToString();
+                listBox1.Items.Add(LP.Secilenadet);
+                listBox2.Items.Add(LP.Ad);
+                listBox3.Items.Add(KDV_3);
+
+            }
+
+            Ceptel TL = new Ceptel("Cep-Telefon", "Samsung", "S8", "edge", 2500, Convert.ToInt32(numericUpDown4.Value), 64, 4, 3400);
+            TL.StokAdedi = Convert.ToInt32(label16.Text) - TL.Secilenadet;
+            double KDV_4 = TL.Hamfiyat * TL.Secilenadet * 1.20;
+
+            if (TL.Secilenadet <= TL.StokAdedi && TL.Secilenadet > 0)
+            {
+                label16.Text = TL.StokAdedi.ToString();
+                listBox1.Items.Add(TL.Secilenadet);
+                listBox2.Items.Add(TL.Ad);
+                listBox3.Items.Add(KDV_4);
+            }
+
+            double Toplam_KDV = KDV_1 + KDV_2 + KDV_3 + KDV_4;
+            label25.Text = Toplam_KDV.ToString() + "TL";
+
+
+            decimal sum = numericUpDown1.Value + numericUpDown2.Value + numericUpDown3.Value + numericUpDown4.Value;
+            if (sum != 0)
+                button1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            numericUpDown1.Value = 0;
+            numericUpDown2.Value = 0;
+            numericUpDown3.Value = 0;
+            numericUpDown4.Value = 0;
+
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            listBox3.Items.Clear();
+            label25.Text = "";
+
+            button1.Enabled = true;
+
+        }
+    }
+}
---
+++ Project 3/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev_3
+{
+
+    // ------------------------------  Urun Sinifi -----------------------------------------------
+
+    class Urun
+    {
+        public string Ad;
+        public string Marka;
+        public string Model;
+        public string Ozellik;
+        public int StokAdedi;
+        public int Hamfiyat;
+        public int Secilenadet;
+
+        public Urun(string ad, string marka, string model, string ozellik, int hamfiyat, int sec_adet)
+        {
+
+            Ad = ad;
+            Marka = marka;
+            Model = model;
+            Ozellik = ozellik;
+            Hamfiyat = hamfiyat;
+            Secilenadet = sec_adet;
+
+        }
+    }
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //--------------------------------  Buzdolab Sinifi    -------------------------------------------------------------
+    class Buzdolab : Urun
+    {
+        public int IcHacim;
+        public string EnerjiSinifi;
+
+        public Buzdolab(string ad, string marka, string model, string ozellik, int hamfiyat, int sec_adet, int hacim, string enerji) : base(ad, marka, model, ozellik, hamfiyat, sec_adet)
+        {
+            Random R1 = new Random(DateTime.Now.GetHashCode());
+            StokAdedi = R1.Next(1, 100);
+            IcHacim = hacim;
+            EnerjiSinifi = enerji;
+        }
+    }
+
+
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //-------------------------------   Ceptel Sinifi -------------------------------------------------------------------
+    class Ceptel : Urun
+    {
+        public int Hafiza;
+        public int Ram;
+        public int Pilgucu;
+
+        public Ceptel(string ad, string marka, string model, string ozellik, int hamfiyat, int sec_adet, int hafiza, int ram, int pil) : base(ad, marka, model, ozellik, hamfiyat, sec_adet)
+        {
+            Random R2 = new Random(DateTime.Now.Second);
+            StokAdedi = R2.Next(1, 100);
+            Hafiza = hafiza;
+            Ram = ram;
+            Pilgucu = pil;
+        }
+    }
+
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //-------------------------------   Ledtv Sinifi -------------------------------------------------------------------
+
+    class LedTV : Urun
+    {
+        public int EkranBoyutu;
+        public string EkranCozunurlugu;
+
+        public LedTV(string ad, string marka, string model, string ozellik, int hamfiyat, int sec_adet, int e_boy, string e_coz) : base(ad, marka, model, ozellik, hamfiyat, sec_adet)
+        {
+            Random R3 = new Random(DateTime.Now.Millisecond);
+            StokAdedi = R3.Next(1, 100);
+            EkranBoyutu = e_boy;
+            EkranCozunurlugu = e_coz;
+        }
+    }
+
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //------------------------------   Laptop Sinifi -------------------------------------------------------------------
+    class Laptop : Urun
+    {
+        public int ekran_boyutu;
+        public string ekran_cozunurluk;
+        public int dahili_hafiza;
+        public int ram_kapasitesi;
+        public int pil_gucu;
+
+        public Laptop(string ad, string marka, string model, string ozellik, int hamfiyat, int sec_adet, int e_boy, string e_coz, int hafiza, int ram, int pil) : base(ad, marka, model, ozellik, hamfiyat, sec_adet)
+        {
+            Random R4 = new Random(DateTime.Now.Minute);
+            StokAdedi = R4.Next(1, 100);
+            ekran_boyutu = e_boy;
+            ekran_cozunurluk = e_coz;
+            dahili_hafiza = hafiza;
+            ram_kapasitesi = ram;
+            pil_gucu = pil;
+        }
+
+    }
+
+
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
