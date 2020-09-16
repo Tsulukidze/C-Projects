42473ba294ea9344a6ced8c3a6986732b74cd475
Tsulukidze <62519788+Tsulukidze@users.noreply.github.com>
2020-09-16 20:35:57 +0400

    Add files via upload

---
+++ Project 2/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace Odev_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txt_Toplam_Y = new System.Windows.Forms.TextBox();
+            this.Txt_Toplam_X = new System.Windows.Forms.TextBox();
+            this.label_Toplam = new System.Windows.Forms.Label();
+            this.label_Y = new System.Windows.Forms.Label();
+            this.ListBox_Y = new System.Windows.Forms.ListBox();
+            this.label_X = new System.Windows.Forms.Label();
+            this.button_Son = new System.Windows.Forms.Button();
+            this.TxtBoxY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtBoxX = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ListBox_X = new System.Windows.Forms.ListBox();
+            this.button_ARKADASMI = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Txt_Toplam_Y
+            // 
+            this.Txt_Toplam_Y.Location = new System.Drawing.Point(628, 339);
+            this.Txt_Toplam_Y.Name = "Txt_Toplam_Y";
+            this.Txt_Toplam_Y.Size = new System.Drawing.Size(120, 22);
+            this.Txt_Toplam_Y.TabIndex = 38;
+            this.Txt_Toplam_Y.Visible = false;
+            // 
+            // Txt_Toplam_X
+            // 
+            this.Txt_Toplam_X.Location = new System.Drawing.Point(487, 339);
+            this.Txt_Toplam_X.Name = "Txt_Toplam_X";
+            this.Txt_Toplam_X.Size = new System.Drawing.Size(120, 22);
+            this.Txt_Toplam_X.TabIndex = 37;
+            this.Txt_Toplam_X.Visible = false;
+            // 
+            // label_Toplam
+            // 
+            this.label_Toplam.AutoSize = true;
+            this.label_Toplam.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Toplam.Location = new System.Drawing.Point(339, 339);
+            this.label_Toplam.Name = "label_Toplam";
+            this.label_Toplam.Size = new System.Drawing.Size(125, 24);
+            this.label_Toplam.TabIndex = 36;
+            this.label_Toplam.Text = "Toplamlar:";
+            this.label_Toplam.Visible = false;
+            // 
+            // label_Y
+            // 
+            this.label_Y.AutoSize = true;
+            this.label_Y.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_Y.Location = new System.Drawing.Point(669, 87);
+            this.label_Y.Name = "label_Y";
+            this.label_Y.Size = new System.Drawing.Size(26, 25);
+            this.label_Y.TabIndex = 35;
+            this.label_Y.Text = "Y";
+            this.label_Y.Visible = false;
+            // 
+            // ListBox_Y
+            // 
+            this.ListBox_Y.FormattingEnabled = true;
+            this.ListBox_Y.ItemHeight = 16;
+            this.ListBox_Y.Location = new System.Drawing.Point(628, 132);
+            this.ListBox_Y.Name = "ListBox_Y";
+            this.ListBox_Y.Size = new System.Drawing.Size(120, 180);
+            this.ListBox_Y.TabIndex = 34;
+            this.ListBox_Y.Visible = false;
+            // 
+            // label_X
+            // 
+            this.label_X.AutoSize = true;
+            this.label_X.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_X.Location = new System.Drawing.Point(528, 87);
+            this.label_X.Name = "label_X";
+            this.label_X.Size = new System.Drawing.Size(27, 25);
+            this.label_X.TabIndex = 33;
+            this.label_X.Text = "X";
+            this.label_X.Visible = false;
+            // 
+            // button_Son
+            // 
+            this.button_Son.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_Son.Location = new System.Drawing.Point(214, 267);
+            this.button_Son.Name = "button_Son";
+            this.button_Son.Size = new System.Drawing.Size(85, 39);
+            this.button_Son.TabIndex = 32;
+            this.button_Son.Text = "SON";
+            this.button_Son.UseVisualStyleBackColor = false;
+            this.button_Son.Click += new System.EventHandler(this.button_Son_Click);
+            // 
+            // TxtBoxY
+            // 
+            this.TxtBoxY.Location = new System.Drawing.Point(117, 194);
+            this.TxtBoxY.Name = "TxtBoxY";
+            this.TxtBoxY.Size = new System.Drawing.Size(100, 22);
+            this.TxtBoxY.TabIndex = 31;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(53, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 25);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Y";
+            // 
+            // TxtBoxX
+            // 
+            this.TxtBoxX.Location = new System.Drawing.Point(117, 139);
+            this.TxtBoxX.Name = "TxtBoxX";
+            this.TxtBoxX.Size = new System.Drawing.Size(100, 22);
+            this.TxtBoxX.TabIndex = 29;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(53, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 25);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "X";
+            // 
+            // ListBox_X
+            // 
+            this.ListBox_X.FormattingEnabled = true;
+            this.ListBox_X.ItemHeight = 16;
+            this.ListBox_X.Location = new System.Drawing.Point(487, 132);
+            this.ListBox_X.Name = "ListBox_X";
+            this.ListBox_X.Size = new System.Drawing.Size(120, 180);
+            this.ListBox_X.TabIndex = 27;
+            this.ListBox_X.Visible = false;
+            // 
+            // button_ARKADASMI
+            // 
+            this.button_ARKADASMI.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button_ARKADASMI.Location = new System.Drawing.Point(58, 267);
+            this.button_ARKADASMI.Name = "button_ARKADASMI";
+            this.button_ARKADASMI.Size = new System.Drawing.Size(124, 39);
+            this.button_ARKADASMI.TabIndex = 26;
+            this.button_ARKADASMI.Text = " ARKADASMI";
+            this.button_ARKADASMI.UseVisualStyleBackColor = false;
+            this.button_ARKADASMI.Click += new System.EventHandler(this.button_ARKADASMI_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Txt_Toplam_Y);
+            this.Controls.Add(this.Txt_Toplam_X);
+            this.Controls.Add(this.label_Toplam);
+            this.Controls.Add(this.label_Y);
+            this.Controls.Add(this.ListBox_Y);
+            this.Controls.Add(this.label_X);
+            this.Controls.Add(this.button_Son);
+            this.Controls.Add(this.TxtBoxY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtBoxX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ListBox_X);
+            this.Controls.Add(this.button_ARKADASMI);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txt_Toplam_Y;
+        private System.Windows.Forms.TextBox Txt_Toplam_X;
+        private System.Windows.Forms.Label label_Toplam;
+        private System.Windows.Forms.Label label_Y;
+        private System.Windows.Forms.ListBox ListBox_Y;
+        private System.Windows.Forms.Label label_X;
+        private System.Windows.Forms.Button button_Son;
+        private System.Windows.Forms.TextBox TxtBoxY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtBoxX;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox ListBox_X;
+        private System.Windows.Forms.Button button_ARKADASMI;
+    }
+}
+
---
+++ Project 2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ARKADASMI_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(TxtBoxX.Text);
+
+            int toplam_x = 0;
+            ListBox_X.Items.Clear();
+            Txt_Toplam_X.Text = "";
+            for (int i = 1; i < x; i++)
+            {
+                if (x % i == 0)
+                {
+                    ListBox_X.Items.Add(i.ToString());
+                    toplam_x += i;
+                }
+                Txt_Toplam_X.Text = toplam_x.ToString();
+            }
+
+
+            int y = Convert.ToInt32(TxtBoxY.Text);
+            int toplam_y = 0;
+            ListBox_Y.Items.Clear();
+            Txt_Toplam_Y.Text = "";
+            for (int i = 1; i < y; i++)
+            {
+                if (y % i == 0)
+                {
+                    ListBox_Y.Items.Add(i.ToString());
+                    toplam_y += i;
+                }
+                Txt_Toplam_Y.Text = toplam_y.ToString();
+
+            }
+
+            label_Toplam.Visible = true;
+            label_X.Visible = true;
+            label_Y.Visible = true;
+            ListBox_X.Visible = true;
+            ListBox_Y.Visible = true;
+            Txt_Toplam_X.Visible = true;
+            Txt_Toplam_Y.Visible = true;
+
+        }
+
+        private void button_Son_Click(object sender, EventArgs e)
+        {
+            int sayi1 = Convert.ToInt32(Txt_Toplam_X.Text);
+            int sayi2 = Convert.ToInt32(Txt_Toplam_Y.Text);
+            int sayi3 = Convert.ToInt32(TxtBoxY.Text);
+            int sayi4 = Convert.ToInt32(TxtBoxX.Text);
+
+            if (sayi1 == sayi3 && sayi2 == sayi4)
+            {
+                MessageBox.Show("Girdiginiz sayilar Arkadasdir.");
+            }
+
+            else
+            {
+                MessageBox.Show("Girdiniz sayilar Arkadas degildir.");
+            }
+        }
+    }
+}
